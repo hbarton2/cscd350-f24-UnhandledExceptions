305c770e8979e846383ffa3cb84aa2c659639e14
Ryan Hirte <rhirte@ewu.edu>
2024-10-09 19:38:49 -0700

    RelationshipItem class done
    Need getName() from ClassItem and then should have no issues

--- RelationshipItem.java
+++ RelationshipItem.java
@@ -1,11 +1,39 @@
 public class RelationshipItem
 {
-	String source = "we storing a unique ID of a classItem here?";
-	String dest = "or actual object references?";
-	//ClassItem dest = new ClassItem()	??
-	
-    public static void RelationshipItem()
-    {
-        //nothing
+    // ClassItem objects that represent the source and destination of the relationship
+    private ClassItem source;
+    private ClassItem destination;
+    
+    public RelationshipItem(final ClassItem source, final ClassItem destination){
+
+        // checking for null values and throwing an exception if the source or destination are null
+        // we can't have a relationship with only a source or only a destination
+        if(source == null || destination == null){
+            throw new IllegalArgumentException("source and destination must not be null");
+        }
+
+        // sets the source and destination ClassItem objects
+        this.source = source;
+        this.destination = destination;
     }
+
+    //returns the source ClassItem object
+    public ClassItem getSource() {
+        return source;
+    }
+
+    //returns the destination ClassItem object
+    public ClassItem getDestination() {
+        return destination;
+    }
+
+    // overrides the toString method to return a string representation of the relationship
+    @Override
+    public String toString(){
+
+        //the ClassItem objects have a getName() method that returns the name of the class
+        return this.source.getName() + "has a relationship with " + this.destination.getName();
+    }
+
+
 };
