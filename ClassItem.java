2d68c0e2539a2ba6c03d2f35862ca3af3268a7db
Preston Lantzer <73727882+Lantzer@users.noreply.github.com>
2024-10-10 04:25:52 +0000

    Added constructor and createClassItem

--- ClassItem.java
+++ ClassItem.java
@@ -2,11 +2,43 @@ import java.util.List;
 	
 public class ClassItem
 {	
+    String name;
+
+    List<FieldItem> fieldItems;
+
+    List<MethodItem> methodItems;
 	//List<FieldItem> fieldItem
 	//List<MethodItem> methodItem
+    
 	
     public static void ClassItem()
     {
         //nothing
     }
+    
+
+    /* Add class method
+     * Checks name is valid
+     * 
+     * if valid
+     *  creates classItem object with field list and method list null.
+     *  prints "class created successfully"
+     *  returns classItem object
+     * 
+     * if not valid
+     *  does not create class object
+     *  
+     */
+    private ClassItem(String classItemName){
+        this.name = classItemName;
+        this.fieldItems = new ArrayList<>();
+        this.methodItems = new ArrayList<>();
+    }
+
+    //returns a class object, to be added to the map in Main.java
+    public ClassItem createClassItem(String classItemName){
+        ClassItem createdClass = new ClassItem(classItemName);
+        return createdClass;
+    }
+    
 };
