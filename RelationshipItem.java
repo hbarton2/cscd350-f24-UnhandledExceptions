dedbd3ad28332533b49f6efb267bc2e89403aa11
Ryan Hirte <rhirte@ewu.edu>
2024-10-10 23:26:48 -0700

    Added addRelationship()
    Created the method for adding a relationship.

--- RelationshipItem.java
+++ RelationshipItem.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class RelationshipItem
 {
     // ClassItem objects that represent the source and destination of the relationship
@@ -15,6 +17,35 @@ public class RelationshipItem
         // sets the source and destination ClassItem objects
         this.source = source;
         this.destination = destination;
+    }
+
+    /* addRelationship takes a Map of relationships we currently have created from the main.java, a ClassItem source and a ClassItem destination.
+     * The goal is to create a relationship object between the source and destination and store it in the map.
+     * If the relationship already exists, we return a message that the relationship already exists.
+     */
+    public String addRelationship(Map<String, RelationshipItem> map, final ClassItem source, final ClassItem destination){
+        // checking for null values and throwing an exception if the source or destination are null
+        // we can't have a relationship with only a source or only a destination
+        if (source == null || destination == null) {
+            throw new IllegalArgumentException("source and destination must not be null");
+        }
+
+        // creating the key for the relationship
+        String key = source.getName() + "_" + destination.getName();
+
+        // checking if the relationship is already created and stored in the map
+        if (map.containsKey(key)) {
+            return "Relationship already exists";
+        }
+
+        // creating the relationship
+        RelationshipItem relationship = new RelationshipItem(source, destination);
+
+        // storing the relationship in the map
+        map.put(key, relationship);
+
+        // returning a message that the relationship has been created
+        return "A relationship has been created between" + source.getName() + " and " + destination.getName();
     }
 
     //returns the source ClassItem object
