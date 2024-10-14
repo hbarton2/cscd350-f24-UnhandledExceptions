28c0fdee1a02529bb8fc88f3a4aec51975d67d8a
Ryan Hirte <rhirte@ewu.edu>
2024-10-13 19:49:41 -0700

    added removeRelationship() to RelationshipItem
    Added a method to remove a relationship.

--- RelationshipItem.java
+++ RelationshipItem.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.util.Scanner;
 
 public class RelationshipItem
 {
@@ -46,6 +47,58 @@ public class RelationshipItem
 
         // returning a message that the relationship has been created
         return "A relationship has been created between" + source.getClassItemName() + " and " + destination.getClassItemName();
+    }
+    /*
+     * removeRelationship takes a Map of relationships we currently have created from the main.java.
+     * This method will list the relationships and prompt the user to remove one by typing the "key" or name of the relationship
+     * If the relationship exists, it will be removed from the map and a message will be returned that the relationship has been removed.
+     * If the relationship is typed incorrectly, a message will be returned that the relationship does not exist. The user will be prompted again.
+     * If the user types "exit", the method will return a message that the user has exited the method, and the method ends.
+     */
+    public String removeRelationship(Map<String, RelationshipItem> relationshipMap){
+        // can't have a null relationshipMap
+        if(relationshipMap == null) {
+            throw new IllegalArgumentException("relationshipMap must not be null");
+        }
+        // can't delete anything if the map is empty
+        if(relationshipMap.isEmpty()){
+            return "No relationships to remove";
+        }
+
+        // printing the relationships
+        System.out.println("Relationships:");
+        // for each loop with entry being the key and value of the map given by relationshipMap.entryset()
+        for(Map.Entry<String, RelationshipItem> entry : relationshipMap.entrySet()){
+            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue().toString());
+        }
+
+        // prompt the user to type the key of the relationship they want to remove
+        // scanner for scanning keyboard
+        Scanner kb = new Scanner(System.in);
+
+        // loop to keep prompting the user until they type "exit" or a valid key
+        while(true){
+            // getting input
+            System.out.println("Enter the key of the relationship to remove (or type 'exit' to cancel):");
+            String key = kb.nextLine();
+
+            // if the user types exit we exit the method
+            if(key.equalsIgnoreCase("exit")){
+                kb.close();
+                return "exiting delete menu...";
+            }
+            
+            // if the relationship exists, we remove it and return a message that the relationship has been removed
+            if(relationshipMap.containsKey(key)){
+                relationshipMap.remove(key);
+                kb.close();
+                return "Relationship has been removed";
+            }
+            // if the relationship does not exist, we return a message that the relationship does not exist
+            else{
+                System.out.println("Please enter a valid option...");
+            }
+        }
     }
 
     //returns the source ClassItem object
