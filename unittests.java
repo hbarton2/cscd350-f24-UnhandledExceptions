94b6bb9eb323978f65ecf8308533412616508dbb
Ryan Hirte <rhirte@ewu.edu>
2024-10-19 20:56:32 -0700

    Added third unit test for listing class info

--- unittests.java
+++ unittests.java
@@ -106,4 +106,31 @@ public class unittests {
     assertEquals(expected, ui.ListClass(classes.get("class2"), relationships));
   }
 
+  // tests that listing class info gives the right output
+  @Test
+  public void testListClassInfoRelationshipwithdifferentclass() {
+    // map of class and relationship items
+    HashMap<String, ClassItem> classes = new HashMap<>();
+    HashMap<String, RelationshipItem> relationships = new HashMap<>();
+
+    // create a class item
+    ClassItem.addClassItem(classes, "class1");
+    ClassItem.addClassItem(classes, "class2");
+    ClassItem.addClassItem(classes, "class3");
+
+    // add a method to class1
+    classes.get("class1").addMethod("method1");
+
+    // create a relationship between the two classes
+    RelationshipItem relationship = new RelationshipItem(classes.get("class2"), classes.get("class3"));
+    relationships.put("class2_class3", relationship);
+
+    // expected output
+    String expected = "\nClass: class1\nFields: not yet\nMethods:\n\tMethod: method1 Parameters: \nRelationships: \n";
+
+    // I have to create a new UI using the default constructor because the ListClass method is not static, this is just to test.
+    UI ui = new UI();
+    assertEquals(expected, ui.ListClass(classes.get("class1"), relationships));
+  }
+
 }
