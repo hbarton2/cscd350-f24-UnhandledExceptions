4c652c937b23286ae147c50724cac82d3701bc5c
Zachary Markley <zmarkley@ewu.edu>
2024-10-23 21:40:14 -0700

    NOT WORKING DO NOT PULL!!! creates a GUI window that does nothing

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class GUI
 {
     Data data = new Data();
@@ -8,7 +11,21 @@ public class GUI
     }
 
     public void Run()
-    {
+    {//game loop for swift?
+        //System.out.println("Can this be reached?");
+        //MOVE THIS CODE TO INIT FUNCTION AND WORK INTO A GAME LOOP TO STOP PRINTING INFINITELY
+        JFrame frame = new JFrame();
+        JButton button = new JButton("Click me");
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Unhandled Exceptions UML");
+        frame.pack();
+        frame.setVisible(true);
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,22 @@ public class Main
 		System.out.print("1. CLI\n2. GUI\n>");
 
 		Scanner scanner = new Scanner(System.in);
-		if (scanner.nextLine().equals("1"))
+		String input = scanner.nextLine();
+		if (input.equals("1"))
 		{
 			CLI cli = new CLI(new Data());
 			while (true)
 				cli.Run();
 		}
-		else
+		else if (input.equals("2"))
 		{
 			GUI gui = new GUI(new Data());
-			while (true)
+			while(true)
 				gui.Run();
+		}
+		else
+		{
+			System.err.println("Invalid input. Try again.");
 		}
 	}
 
