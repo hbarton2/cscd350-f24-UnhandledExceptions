90f248a73f1c694561b95ec46dc62885836cc88d
Ryan Hirte <rhirte@ewu.edu>
2024-10-29 15:35:43 -0700

    added relationship test file

---
+++ uml/src/test/java/RelationshipItemTests.java
@@ -0,0 +1,8 @@
+// Testing imports needed
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+public class RelationshipItemTests {
+  
+}
