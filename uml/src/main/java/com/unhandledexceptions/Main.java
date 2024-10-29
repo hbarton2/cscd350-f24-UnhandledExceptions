21c1eddfe161566f076f9b588cdc31af3332c044
Zachary Markley <129787363+zDmarkley@users.noreply.github.com>
2024-10-28 23:38:45 -0700

    Re-factored Main to require letter inputs rather than numbers and an error message for an incorrect input

--- uml/src/main/java/com/unhandledexceptions/Main.java
+++ uml/src/main/java/com/unhandledexceptions/Main.java
@@ -12,18 +12,23 @@ public class Main
 	public static void main(String[] args)
 	{
 		System.out.println("\n-=Unhandled Exceptions UML Editor=-");
-		System.out.print("1. CLI\n2. GUI\n>");
+		System.out.print("1. CLI (type cli or c)\n2. GUI (type gui or g)\n>");
 
 		Scanner scanner = new Scanner(System.in);
-		if (scanner.nextLine().equals("1"))
+		String input = scanner.nextLine().toLowerCase();
+		if (input.equals("cli") || input.equals("c"))
 		{
 			CLI cli = new CLI(new Data());
 			while (true)
 				cli.Run();
 		}
-		else
+		else if(input.equals("gui") || input.equals("g"))
 		{
 			GUI.main();
+		}
+		else
+		{
+			System.err.println("Invalid Input. please re-run the program and try again.");
 		}
 		scanner.close();
 	}
