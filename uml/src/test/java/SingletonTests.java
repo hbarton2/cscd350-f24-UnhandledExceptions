a63b3bc6fece1ed36c59b84a66f04d9d08380e8e
Ryan Hirte <rhirte@ewu.edu>
2024-11-07 17:14:42 -0800

    singleton is proper for CLI

---
+++ uml/src/test/java/SingletonTests.java
@@ -0,0 +1,30 @@
+// testing imports
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+// modules being tested for singleton
+import com.unhandledexceptions.View.CLI;
+import com.unhandledexceptions.View.GUI;
+import com.unhandledexceptions.Model.Data;
+
+public class SingletonTests {
+  @Test
+  public void testCLISingleton() {
+    // Get the first instance of CLI
+    CLI instance1 = CLI.getInstance(new Data());
+    // Get the second instance of CLI
+    CLI instance2 = CLI.getInstance(new Data());
+
+    // Assert that both instances are the same
+    assertSame(instance1, instance2, "CLI instances are not the same");
+  }
+
+  @Test
+  public void testCLINotNull() {
+    // Get the instance of CLI
+    CLI instance = CLI.getInstance(new Data());
+
+    // Assert that the instance is not null
+    assertNotNull(instance, "CLI instance is null");
+    }
+}
