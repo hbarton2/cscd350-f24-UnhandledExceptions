a9a473fb4a96c6555ffc59ccbefae82acce93098
Ryan Hirte <rhirte@ewu.edu>
2024-11-10 11:22:29 -0800

    caretaker saveState unit test passes

---
+++ uml/src/main/java/com/unhandledexceptions/Controller/Caretaker.java
@@ -0,0 +1,37 @@
+package com.unhandledexceptions.Controller;
+
+import com.unhandledexceptions.Model.Data;
+import com.unhandledexceptions.Model.Data.Memento;
+
+import java.util.Stack;
+/*
+ * This is the Caretaker class for the Memento design pattern. It is responsible for storing the memento objects.
+ * It is also responsible for restoring the state of the Data object.
+ */
+public class Caretaker {
+    // Data is a field because our caretaker will only manage one Data object throughout the program
+    // Because of this, tight coupling between Data and Caretaker seems okay.
+    // undoStack and redoStack are stacks that store the memento objects
+    private Data data;
+    private Stack<Memento> undoStack;
+    private Stack<Memento> redoStack;
+
+    // Constructor
+    public Caretaker(Data data) {
+        this.undoStack = new Stack<>();
+        this.redoStack = new Stack<>();
+        this.data = data;
+    }
+
+    // Save the current state of the Data object
+    public void saveState() {
+        undoStack.push(this.data.createMemento());
+        redoStack.clear(); // Clear redo stack whenever a new state is saved
+    }
+
+    // getters for the stacks
+    // ONLY for testing, I don't see a reason we need to call these outside of unit tests
+    public Stack<Memento> getUndoStack() {
+        return undoStack;
+    }
+}
