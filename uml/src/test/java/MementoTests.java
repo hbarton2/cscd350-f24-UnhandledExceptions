42020a018945163e28f35a093ee45624e9e8cad7
Ryan Hirte <rhirte@ewu.edu>
2024-11-09 20:48:17 -0800

    restore from momento test created

--- uml/src/test/java/MementoTests.java
+++ uml/src/test/java/MementoTests.java
@@ -68,4 +68,40 @@ public class MementoTests {
     assertEquals(testMemento.getRelationshipItems(), memento.getRelationshipItems());
   }
 
+  // Test that a memento was properly restored
+  @Test
+  public void restoreFromMementoTest() {
+    Data data = new Data();
+    HashMap<String, ClassItem> classes = new HashMap<>();
+    HashMap<String, RelationshipItem> relationships = new HashMap<>();
+    
+    // create classes and relationships for memento
+    ClassItem.addClassItem(classes, "class1");
+    ClassItem.addClassItem(classes, "class2");
+    RelationshipItem.addRelationship(classes, relationships, "class1", "class2", "compisition");
+
+    // set the maps in data
+    data.setClassItems(classes);
+    data.setRelationshipItems(relationships);
+
+    // Test the constructor creates a Memento object
+    // data.new because Memento is nested in Data.java
+    Memento testMemento = data.new Memento(data.getClassItems(), data.getRelationshipItems());
+
+    // create a new class to change the state of data
+    ClassItem.addClassItem(classes, "class3");
+
+    // set the maps in data
+    data.setClassItems(classes);
+    
+    // Make sure data contains class3 now.
+    assertTrue(data.getClassItems().containsKey("class3"));
+
+    // restore the state of data from the memento
+    data.restoreFromMemento(testMemento);
+    
+    // At this point class3 should NOT exist in data
+    assertFalse(data.getClassItems().containsKey("class3"));
+  }
+
 }
