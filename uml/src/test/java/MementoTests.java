6fc1e8de1b2939e9f469a7af3965599434b4870a
Ryan Hirte <rhirte@ewu.edu>
2024-11-09 20:27:54 -0800

    unit test for Data createMemento()

--- uml/src/test/java/MementoTests.java
+++ uml/src/test/java/MementoTests.java
@@ -29,7 +29,7 @@ public class MementoTests {
 
     // Test the constructor creates a Memento object
     // data.new because Memento is nested in Data.java
-    Memento testMemento = data.new Memento(data, data.getClassItems(), data.getRelationshipItems());
+    Memento testMemento = data.new Memento(data.getClassItems(), data.getRelationshipItems());
 
     // Verify the memento stores the correct state
     assertEquals(data.getClassItems(), testMemento.getClassItems());
@@ -39,6 +39,33 @@ public class MementoTests {
     assertTrue(data.getClassItems().get("class1").equals(testMemento.getClassItems().get("class1")));
     assertTrue(data.getClassItems().get("class2").equals(testMemento.getClassItems().get("class2")));
     assertTrue(data.getRelationshipItems().get("class1_class2").equals(testMemento.getRelationshipItems().get("class1_class2")));
+  }
+
+  // Test that a memento was properly created in Data.java
+  @Test
+  public void createMementoTest() {
+    Data data = new Data();
+    HashMap<String, ClassItem> classes = new HashMap<>();
+    HashMap<String, RelationshipItem> relationships = new HashMap<>();
+    
+    // create classes and relationships for memento
+    ClassItem.addClassItem(classes, "class1");
+    ClassItem.addClassItem(classes, "class2");
+    RelationshipItem.addRelationship(classes, relationships, "class1", "class2", "compisition");
+
+    // set the maps in data
+    data.setClassItems(classes);
+    data.setRelationshipItems(relationships);
+
+    // Test the constructor creates a Memento object
+    // data.new because Memento is nested in Data.java
+    Memento testMemento = data.new Memento(data.getClassItems(), data.getRelationshipItems());
+
+    Memento memento = data.createMemento();
+
+    // test the memento created from createMemento() is the same as the one created in the constructor
+    assertEquals(testMemento.getClassItems(), memento.getClassItems());
+    assertEquals(testMemento.getRelationshipItems(), memento.getRelationshipItems());
   }
 
 }
