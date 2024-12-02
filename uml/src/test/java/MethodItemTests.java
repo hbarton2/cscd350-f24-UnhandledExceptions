d7634e6c68d0831ab008c7a7f923e52e424188ad
Ryan Hirte <rhirte@ewu.edu>
2024-12-01 20:26:21 -0800

    good coverage for MethodItem

--- uml/src/test/java/MethodItemTests.java
+++ uml/src/test/java/MethodItemTests.java
@@ -1,10 +1,10 @@
 // Testing imports
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.BeforeEach;
 
 import com.unhandledexceptions.Model.MethodItem;
 import com.unhandledexceptions.Model.ParameterItem;
+import java.util.HashMap;
 
 
 public class MethodItemTests {
@@ -125,4 +125,180 @@ public class MethodItemTests {
         assertFalse(methodItem.getParameters().containsKey("parameterName"));
     }
 
+    // Test removing a param that doesn't exist
+    @Test
+    public void testRemoveParameterDoesNotExist() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // remove the parameter
+        String result = MethodItem.removeParameter(methodItem, "type", "parameterName");
+        // Assert we have the right result
+        assertEquals("Parameter: type parameterName does not exist", result);
+    }
+
+    // Test removing a param with a null name
+    @Test
+    public void testRemoveNullParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Ensure that an IllegalArgumentException is thrown when the parameter name is null
+        assertThrows(IllegalArgumentException.class, () -> {
+            MethodItem.removeParameter(methodItem, "type", null);
+        });
+    }
+
+    // Test changing a parameter
+    @Test
+    public void testChangeParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // add a parameter
+        MethodItem.addParameter(methodItem, "type", "parameterName");
+        // Assert we have the parameter
+        assertTrue(methodItem.getParameters().containsKey("parameterName"));
+        // change the parameter
+        MethodItem.changeParameter(methodItem, "type", "parameterName", "newType", "newName");
+        // Assert we don't have the parameter anymore
+        assertFalse(methodItem.getParameters().containsKey("parameterName"));
+        // Assert we have the right parameter
+        assertEquals("newName", methodItem.getParameters().get("newName").getName());
+    }
+
+    // Test if renaming parameter is already in use
+    @Test
+    public void testChangeParameterInUse() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // add a parameter
+        MethodItem.addParameter(methodItem, "type", "parameterName");
+        // Assert we have the parameter
+        assertTrue(methodItem.getParameters().containsKey("parameterName"));
+        // change the parameter
+        String result = MethodItem.changeParameter(methodItem, "type", "parameterName", "newType", "parameterName");
+        // Assert we have the right result
+        assertEquals("New Parameter name: parameterName already in use.", result);
+        // Assert we still have the same parameter
+        assertTrue(methodItem.getParameters().containsKey("parameterName"));
+    }
+    
+    // Test changing nonexistent parameter
+    @Test
+    public void testChangeParameterDoesntExist() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // change the parameter
+        String result = MethodItem.changeParameter(methodItem, "type", "parameterName", "newType", "newName");
+        // Assert we have the right result
+        assertEquals("Parameter: type parameterName does not exist.", result);
+    }
+
+    // Test changing a parameter with a null name
+    @Test
+    public void testChangeNullParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Ensure that an IllegalArgumentException is thrown when the parameter name is null
+        assertThrows(IllegalArgumentException.class, () -> {
+            MethodItem.changeParameter(methodItem, "type", null, "newType", "newName");
+        });
+    }
+
+    // Test renaming parameter
+    @Test
+    public void testRenameParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // add a parameter
+        MethodItem.addParameter(methodItem, "type", "parameterName");
+        // rename the parameter
+        MethodItem.renameParameter(methodItem, "parameterName", "newName");
+        // assert we have the new name and not the old name
+        assertFalse(methodItem.getParameters().containsKey("parameterName"));
+        assertTrue(methodItem.getParameters().containsKey("newName"));
+    }
+
+    // Test renaming parameter that doesn't exist
+    @Test
+    public void testRenameParameterNotExist() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // rename parameter
+        String result = MethodItem.renameParameter(methodItem, "blaj", "slkja;dls");
+        // make sure we have correct output
+        assertEquals("Parameter: blaj does not exist.", result);
+    }
+
+    // Test null input for name of parameter
+    @Test
+    public void testRenameNullParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Ensure that an IllegalArgumentException is thrown when the parameter name is null
+        assertThrows(IllegalArgumentException.class, () -> {
+            MethodItem.renameParameter(methodItem, null, "newName");
+        });
+    }
+
+    // Test retype parameter
+    @Test
+    public void testRetypeParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Add parameter
+        MethodItem.addParameter(methodItem, "type", "parameterName");
+        // change the type
+        MethodItem.retypeParameter(methodItem, "parameterName", "int");
+        // assert we have the correct type
+        assertEquals("int", methodItem.getParameter("parameterName").getType());
+        assertNotEquals("type", methodItem.getParameter("parameterName").getType());
+    }
+
+    // Test retype parameter that doesn't exist
+    @Test
+    public void testRetypeParameterNotExist() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // retype parameter
+        String result = MethodItem.retypeParameter(methodItem, "parameterName", "int");
+        // make sure we have correct output
+        assertEquals("Parameter: parameterName does not exist.", result);
+    }
+    // Test null input for retype parameter
+    @Test
+    public void testRetypeNullParameter() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Ensure that an IllegalArgumentException is thrown when the parameter name is null
+        assertThrows(IllegalArgumentException.class, () -> {
+            MethodItem.retypeParameter(methodItem, null, "int");
+        });
+    }
+
+    // Test toString
+    @Test
+    public void toStringTest() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Add parameter
+        MethodItem.addParameter(methodItem, "type", "parameterName");
+        // Expected result
+        String expected = "Method: type methodName Parameters: [type parameterName] ";
+        // Assert we have the right string
+        assertEquals(expected, methodItem.toString());
+    }
+
+    // Test setParameters 
+    @Test
+    public void testSetParameters() {
+        // Create the MethodItem
+        MethodItem methodItem = new MethodItem("methodName", "type");
+        // Create a new HashMap
+        HashMap<String, ParameterItem> parameters = new HashMap<>();
+        // Add a parameter
+        parameters.put("parameterName", new ParameterItem("parameterName", "type"));
+        // Set the parameters
+        methodItem.setParameters(parameters);
+        // Assert we have the right parameters
+        assertEquals("parameterName", methodItem.getParameters().get("parameterName").getName());
+    }
 }
